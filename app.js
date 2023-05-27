bced5cdcf709e215dbed326f201167034cb07924
AlbertineN <albertinengingan@gmail.com>
2023-05-27 14:05:43 +0300

    modified app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const os = require('os');
 
-console.log("Albertine-Devops Test Server Starting...");
+console.log("Moringa-DevOps Test server starting...");
 
-var handler = function(request, respond){
-    console.log("received request from" + request.connection.remoteAddress);
-    response.writeHead(200);
-    response.end("You've hit " + os.hostname() + "\n"); 
+var handler = function(request, response) {
+ console.log("Received request from " + request.connection.remoteAddress);
+ response.writeHead(200);
+ response.end("You've hit " + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
